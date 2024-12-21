d58447614daa8b6ace257d0d7342bebbd3584715
msrock10 <madihazafar58@gmail.com>
2024-12-21 02:03:34 -0800

    smsms

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-var index=0;
-function carousel(){
-    var i;
-    var x = document.getElementsByClassName("my-slides");
-}
-for (i = 0; i < x.length; i++) {``
+var index = 0;
 
-    x[i].style.display = "none";
-    
-    }
-    index=++
-    x[index - 1].style.display = "block";
-    setTimeout(carousel,6000);
-    carousel();
+function carousel() {
+
+var i;
+
+var x = document.getElementsByClassName("myslides");
+
+for (i = 0; i < x.length; i++) {
+
+x[i].style.display = "none";
+
+}
+
+index++;
+
+if (index > x.length) {
+
+index = 1;
+
+}
+
+x[index - 1].style.display = "block";
+
+setTimeout(carousel, 2000);
+
+}
+
+carousel(); // Start the carousel when the page loads
